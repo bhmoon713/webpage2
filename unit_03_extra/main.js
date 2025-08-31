29a95772ea86a9b14ff583493a2fd1cdbe80d850
bhmoon <bhmoon713@gmail.com>
2025-08-31 00:28:54 +0000

    adding correct 03 extra

--- unit_03_extra/main.js
+++ unit_03_extra/main.js
@@ -1,9 +1,9 @@
 let vueApp = new Vue({
-    el: "#vueApp",
+    el: "#app",
     data: {
         // ros connection
         ros: null,
-        rosbridge_address: 'wss://i-0734dfc7411934198.robotigniteacademy.com/rosbridge/',
+        rosbridge_address: 'wss://i-0c34017576fd20f71.robotigniteacademy.com/4f4f113a-01d0-43db-8f40-b6c1394290d7/rosbridge/',
         connected: false,
         // page content
         menu_title: 'Connection',
@@ -23,7 +23,9 @@ let vueApp = new Vue({
         joystick: {
             vertical: 0,
             horizontal: 0,
-        }
+        },
+        // publisher
+        pubInterval: null,
     },
     methods: {
         connect: function() {
@@ -36,6 +38,7 @@ let vueApp = new Vue({
             this.ros.on('connection', () => {
                 this.connected = true
                 console.log('Connection to ROSBridge established!')
+                this.pubInterval = setInterval(this.publish, 100)
             })
             this.ros.on('error', (error) => {
                 console.log('Something went wrong when trying to connect')
@@ -44,7 +47,20 @@ let vueApp = new Vue({
             this.ros.on('close', () => {
                 this.connected = false
                 console.log('Connection to ROSBridge was closed!')
+                clearInterval(this.pubInterval)
             })
+        },
+        publish: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/fastbot/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: this.joystick.vertical, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: this.joystick.horizontal, },
+            })
+            topic.publish(message)
         },
         disconnect: function() {
             this.ros.close()
